27f60d8d3d3c5b03bca1cb28cff3ce30ec44634f
kingvpn77 <155173495+king-vpn@users.noreply.github.com>
2024-03-26 06:30:59 +0700

    Update cf.sh

--- file/cf.sh
+++ file/cf.sh
@@ -2,12 +2,12 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 read -p "Masukan Domain (contoh : Anies7)" domen
-DOMAIN=mypremium.biz.id
+DOMAIN=kingvpn.my.id
 sub=${domen}
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.mypremium.biz.id
-CF_ID=andyyuda41@gmail.com
-CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+dns=${sub}.kingvpn.my.id
+CF_ID=hannaugo@gmail.com
+CF_KEY=e3341a6705e970eda3577f440d0cca6e3d682
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
